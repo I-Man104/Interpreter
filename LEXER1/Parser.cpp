cecdff4f2a619a9501c782e296ed3f35f88cd3de
fatmaawad <76003407+fatmaawad@users.noreply.github.com>
2022-10-08 20:44:58 +0200

    Add files via upload

--- LEXER1/Parser.cpp
+++ LEXER1/Parser.cpp
@@ -15,7 +15,29 @@ void Match(string lexeme) {
 		throw new LexerException("Error");
 	}
 }
+Expression* IntLit::IntLiteral(int val)
+{
+	IntLit* t;
+	t->intVal = val;
+	return t;
+}
 
+Expression* StrLit::StrLiteral(string val)
+{
+	StrLit* t;
+	t->strVal = val;
+	return t;
+}
+/*
+Expression* Expr::expr(string op, Expression* _t1, Expression* _t2)
+{
+	Expr* t;
+	t->operation = op;
+	t->Term1 = _t1;
+	t->Term2 = _t2;
+	return t;
+
+}*/
 Expression* IF::IfExpression(Expression* cond, Expression* thenExp, Expression* elseExp) {
 	IF* t;
 	t->condition = cond;
@@ -29,6 +51,15 @@ Expression* Binary::BinaryPrimitive(string op, Binary* t, Binary* t2) {
 	t->Term1 = t;
 	t->Term2 = t2;
 	return t;
+}
+
+Expression* LetExpr::LetExpression(Expression* t, BindList* lst)
+{
+	LetExpr* T;
+	BindList* lst = Bindings();
+	T->_t = t;
+
+	return T;
 }
 
 void funcExp::FunctionExpression(Expression* exp, ParamList* lst) {
@@ -46,3 +77,20 @@ ParamList* ParamList::Params() {
 	Match(TOKEN_RPAREN);
 	return this;
 }
+BindList* BindList::Bindings()
+{
+}
+
+ArgList* ArgList::Args()
+{
+	Match(TOKEN_LPAREN);
+		while (true)
+		{
+			this->_argLst.push_back(*c.NextToken);
+			Match(TOKEN_COMMA);
+			if (Lookahead->Type == TOKEN_RPAREN) break;
+		}
+		Match(TOKEN_RPAREN);
+		return this;
+}
+
