9067bbb01f529ed8e70e40260c4225fa263b4f82
Ayman013 <ayman.m.h104@gmail.com>
2022-10-08 23:14:01 +0200

    parser done?

--- LEXER1/SeenParser.h
+++ LEXER1/SeenParser.h
@@ -2,11 +2,13 @@
 #ifndef __SEEN_PARSER_H__
 #define __SEEN_PARSER_H__
 #include "Parser.h"
-class SeenParser:Parser
+class SeenParser:public Expression
 {
 	Expression* ArithmeticExpr();
 	Expression* Term();
 	Expression* Primary();
 	Expression* PrimaryNonApplication();
+	Expression* Expr();
 };
+
 #endif
